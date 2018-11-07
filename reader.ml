d2726e79014fcd4896e9db5ab9c69a02dc0ebcac
sharondaniely <sharon.daniely7@gmail.com>
2018-11-07 20:06:49 +0200

    sharon after int and begin of hex

--- reader.ml
+++ reader.ml
@@ -40,6 +40,7 @@ module Reader: sig (*TODO add sig for parsers and then remove*)
   val read_sexpr : string -> sexpr
   val read_sexprs : string -> sexpr list
   val bool_parser : char list -> sexpr * char list
+  val digit_parser : char list -> char * char list
 end
 = struct
 let normalize_scheme_symbol str =
@@ -67,17 +68,75 @@ let char_prefix_parser s =
   prefix_packed s;;
 
 let visible_simple_char_parser s = 
-  let visible_parser = const (fun (temp)-> (int_of_char temp) > 32) in
+  let visible_parser = PC.const (fun (temp)-> (int_of_char temp) > 32) in
   let visiable_packed = PC.pack visible_parser (fun (temp) -> Char(temp)) in
   visiable_packed s;;
 
 let named_char_parser s = 
-  let named_parser = const (fun (temp)-> (int_of_char temp) == 0 || (int_of_char temp) == 10 || (int_of_char temp) == 13 || (int_of_char temp) == 9 || (int_of_char temp) == 12 || (int_of_char temp) == 32 ) in
+  let named_parser = PC.const (fun (temp)-> (int_of_char temp) == 0 
+                                          || (int_of_char temp) == 10 
+                                          || (int_of_char temp) == 13 
+                                          || (int_of_char temp) == 9 
+                                          || (int_of_char temp) == 12 
+                                          || (int_of_char temp) == 32 ) in
   let named_packed = PC.pack named_parser (fun (temp) -> Char(temp)) in
   named_packed s;;
 
-let hex_char_parser s =
-  let hex_parser = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(*******************************numbers**********************************)
+
+
+let digit_parser =  PC.range '0' '9'
+;;
+
+let natural_parser s   =
+let natural_helper = PC.plus digit_parser in
+let packed_natural_parser = PC.pack natural_helper (fun (temp)-> int_of_string (list_to_string temp)) in
+packed_natural_parser s;;
+
+let sign_parser s =
+  let sign_helper = PC.const (fun (temp)-> temp = '-' || temp = '+') in
+    let parser = PC.pack sign_helper (fun (temp)-> temp) in
+    parser s;;
+
+let signed_integer_parser s =
+  let integer_helper = PC.pack (PC.caten sign_parser natural_parser ) (fun (temp)-> if ((fst temp) = '-') 
+                                                                                        then Number(Int ((-1)*(snd temp)))
+                                                                                        else Number(Int (snd temp))) in
+  integer_helper s;;
+
+ let not_signed_integer_parser s =
+    PC.pack natural_parser (fun (temp)-> Number(Int(temp))) s;;
+
+let integer_parser s = (PC.disj signed_integer_parser not_signed_integer_parser) s;;
+
+
+(** *************************************HEX***************************************)
+
+let hex_prefix s = PC.pack (PC.word_ci "#x") (fun (temp)-> Nil) s;;
+
+let hex_natural_parser s = PC.pack (PC.plus hex_digit_parser) s;;
+
+let not_signed_hex_integer_parser s =
+  PC.pack (PC.caten hex_prefix hex_natural_parser) (fun (temp)-> Number(Int (int_of_string ( "0x" ^ (list_to_string(snd temp)))))) s;;
+     
+
+
+
+
 
 
 
