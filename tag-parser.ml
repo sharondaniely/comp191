f08e00d7b02446e4db9606b83c13d2d70ec03c7e
AdiKrasin <adikrasin@gmail.com>
2018-11-22 11:39:47 +0200

    adi added comment for mit define

--- tag-parser.ml
+++ tag-parser.ml
@@ -100,7 +100,8 @@ let rec expr_parser s =
   (*| Pair(Symbol("let*"), x) -> (expr_let_star_parser x) (*TODO WRITE THIS FUNCTION*)
   | Pair(Symbol("letrec"), x) -> (expr_letrec_parser x) (*TODO WRITE THIS FUNCTION*)*)
   | Pair(Symbol("and"), x) -> (and_expr_parser x)
-  (*| Pair(Symbol("quasiquote"), Pair(x , Nil)) -> (quasiquote_expr_parser x) (*TODO WRITE THIS FUNCTION*)*)
+  (*| Pair(Symbol("quasiquote"), Pair(x , Nil)) -> (quasiquote_expr_parser x) (*TODO WRITE THIS FUNCTION*)
+  MIT Define TODO *)
   | Pair(a , b) -> Applic((expr_parser a) , (nested_pair_sexpr_to_list b))
   | _ -> raise X_syntax_error
  and or_expr_parser x =
