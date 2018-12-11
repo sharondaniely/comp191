718e65323ec532b65a373bdd0139bf77a9c094b1
AdiKrasin <adikrasin@gmail.com>
2018-12-11 16:59:26 +0200

    adi lexical

--- semantic-analyser.ml
+++ semantic-analyser.ml
@@ -67,8 +67,33 @@ end;;
 
 module Semantics : SEMANTICS = struct
 
-let annotate_lexical_addresses e = raise X_not_yet_implemented;;
 
+
+let rec lexical e plst blst =
+  match e with
+  | Const(c) -> Const'(c)
+  | If(exp1, exp2, exp3) -> If'((lexical exp1 plst blst), (lexical exp2 plst blst), (lexical exp3 plst blst))
+  | Seq(exp_lst) -> Seq'((List.map (fun (exp) -> (lexical exp plst blst)) exp_lst))
+  | Def(exp1, exp2) -> Def'((lexical exp1 plst blst), (lexical exp2 plst blst))
+  | Or(expr_lst) -> Or'((List.map (fun (exp) -> (lexical exp plst blst)) expr_lst))
+  | Applic(exp, expr_lst) -> Applic'((lexical exp plst blst), (List.map (fun (exp1)-> (lexical exp1 plst blst)) expr_lst))
+  | LambdaSimple(str_lst, exp) -> LambdaSimple'(str_lst, (lexical exp str_lst (plst::blst)))
+  | LambdaOpt(str_lst, str, exp) -> LambdaOpt'(str_lst, str, (lexical exp (str_lst@[str]) (plst::blst)))
+  | Var(str) -> if (List.mem str plst)
+                then Var'(VarParam(str, (index_in_lst str plst)))
+                else (bound_or_free str blst 0)
+  | _ -> raise X_syntax_error
+ and index_in_lst str lst =
+  match lst with
+    | [] -> raise X_syntax_error
+    | h :: t -> if str = h then 0 else 1 + (index_in_lst str t)
+ and bound_or_free str lst index=
+  match lst with
+  | [] -> Var'(VarFree(str))
+  | h :: t -> if (List.mem str h) then Var'(VarBound(str, index, (index_in_lst str h))) else (bound_or_free str t (index+1));; 
+  
+let annotate_lexical_addresses e = (lexical e [] [])  
+ 
 let annotate_tail_calls e = raise X_not_yet_implemented;;
 
 let box_set e = raise X_not_yet_implemented;;
