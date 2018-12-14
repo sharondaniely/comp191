0f83fec502533834a33972e83f9227f45db3b40d
AdiKrasin <adikrasin@gmail.com>
2018-12-14 14:00:22 +0200

    adi half of box

--- semantic-analyser.ml
+++ semantic-analyser.ml
@@ -63,6 +63,7 @@ module type SEMANTICS = sig
   val annotate_lexical_addresses : expr -> expr'
   val annotate_tail_calls : expr' -> expr'
   val box_set : expr' -> expr'
+  val get_read_lst : string -> expr' -> int -> bool -> bool -> int list (*TODO DON'T FORGET TO REMOVE *)
 end;;
 
 module Semantics : SEMANTICS = struct
@@ -83,7 +84,6 @@ let rec lexical e plst blst =
   | Var(str) -> if (List.mem str plst)
                 then Var'(VarParam(str, (index_in_lst str plst)))
                 else (bound_or_free str blst 0)
-  | _ -> raise X_syntax_error
  and index_in_lst str lst =
   match lst with
     | [] -> raise X_syntax_error
@@ -94,7 +94,127 @@ let rec lexical e plst blst =
   | h :: t -> if (List.mem str h) then Var'(VarBound(str, index, (index_in_lst str h))) else (bound_or_free str t (index+1));; 
   
 
-let rec tail_calls e in_tp =
+
+let rec box e =
+  match e with
+  | Const'(c) -> Const'(c)
+  | If'(exp1, exp2, exp3) -> If'((box exp1), (box exp2), (box exp3))
+  | Seq'(exp_lst) -> Seq'((List.map (fun (exp) -> (box exp)) exp_lst))
+  | Def'(exp1, exp2) -> Def'((box exp1), (box exp2))
+  | Set'(exp1, exp2) -> Set'((box exp1), (box exp2))
+  | Or'(exp_lst) -> Or'((List.map (fun (exp) -> (box exp)) exp_lst))
+  | Var'(var) -> Var'(var)
+  | Applic'(exp, exp_lst) -> Applic'((box exp), (List.map (fun (exp1) -> (box exp1)) exp_lst))
+  | ApplicTP'(exp, exp_lst) -> ApplicTP'((box exp), (List.map (fun (exp1) -> (box exp1)) exp_lst))
+  | LambdaSimple'(str_lst, exp) -> (box_for_lambdas e)
+  | LambdaOpt'(str_lst, str, exp) -> (box_for_lambdas e)
+  (* TODO WOULD PROBABLY NEED TO DEAL WITH BOX SET BOX GET AND BOX PROBABLY LEAVE THEM THE SAME*)
+  | _ -> raise X_syntax_error
+ and box_for_lambdas lambda =
+   match lambda with
+   | LambdaSimple'(str_lst, Seq'(exp_lst)) -> 
+        (box_lambda_and_send_recursivly lambda (List.map (fun (str) -> (should_be_boxed str lambda)) str_lst))
+   | LambdaOpt'(str_lst, str, Seq'(exp_lst)) -> 
+        (box_lambda_and_send_recursivly lambda (List.map (fun (str1) -> (should_be_boxed str1 lambda)) str_lst@[str]))
+   | LambdaSimple'(str_lst, exp) ->LambdaSimple'(str_lst, (box exp))
+   | LambdaOpt'(str_lst, str, exp) -> LambdaOpt'(str_lst, str, (box exp))
+   | _ -> raise X_syntax_error
+  and should_be_boxed str lambda =
+    let read_lst = (get_read_lst str lambda 0 true true) in
+    let write_lst = (get_write_lst str lambda 0 true true) in
+    let ans_lst1 = (List.map (fun (num) -> (contain_another_num num write_lst)) read_lst) in
+    let ans_lst2 = (List.map (fun (num) -> (contain_another_num num read_lst)) write_lst) in
+    if ((List.mem true ans_lst1) || (List.mem true ans_lst2))
+       then "true"
+       else "false"
+  and contain_another_num num lst =
+    match lst with
+  | [] -> false
+  | h :: t -> if (num = h) then (contain_another_num num t) else true
+  and get_read_lst str exp counter is_first same_str =
+   match exp with
+  | Const'(c) -> []
+  | If'(exp1, exp2, exp3) -> 
+     (get_read_lst str exp1 counter is_first same_str)@(get_read_lst str exp2 counter is_first same_str)@(get_read_lst str exp3 counter is_first same_str)
+  | Seq'(exp_lst) ->
+    (List.fold_left (fun a b -> List.append a b) [] (List.map (fun (exp1) -> (get_read_lst str exp1 counter is_first same_str)) exp_lst))
+  | Def'(exp1, exp2) -> (get_read_lst str exp2 counter is_first same_str)
+  | Set'(exp1, exp2) -> (get_read_lst str exp2 counter is_first same_str) (*TODO CHECK IF IT NEEDS TO BE DIFFERENT HERE FOR SET OF NOT VAR*)
+  | Or'(exp_lst) ->
+     (List.fold_left (fun a b -> List.append a b) [] (List.map (fun (exp1) -> (get_read_lst str exp1 counter is_first same_str)) exp_lst))
+  | Var'(VarFree(str1)) -> []
+  | Var'(VarBound(str1, int1, int2)) -> if (same_str && (str1 = str)) then [counter] else []
+  | Var'(VarParam(str1, int1)) -> if (is_first && (str1 = str)) then [0] else []
+  | Applic'(exp1, exp_lst) -> 
+    (get_read_lst str exp1 counter is_first same_str)@(List.fold_left (fun a b -> List.append a b) [] (List.map (fun (exp2) -> (get_read_lst str exp2 counter is_first same_str)) exp_lst))
+  | ApplicTP'(exp1, exp_lst) -> 
+    (get_read_lst str exp1 counter is_first same_str)@(List.fold_left (fun a b -> List.append a b) [] (List.map (fun (exp2) -> (get_read_lst str exp2 counter is_first same_str)) exp_lst))
+  | LambdaSimple'(str_lst, Seq'(exp_lst)) -> (seq_read_lst str exp_lst counter is_first same_str)
+  | LambdaOpt'(str_lst1, str1, Seq'(exp_lst)) -> (seq_read_lst str exp_lst counter is_first same_str)
+  | LambdaSimple'(str_lst1, LambdaSimple'(str_lst2,exp)) -> if is_first
+                                                           then if (List.mem str str_lst2)
+                                                                then (get_read_lst str (LambdaSimple'(str_lst2,exp)) counter false false)
+                                                                else (get_read_lst str (LambdaSimple'(str_lst2,exp)) counter false same_str)
+                                                           else  if (List.mem str str_lst2)
+                                                                then (get_read_lst str (LambdaSimple'(str_lst2,exp)) counter is_first false)
+                                                                else (get_read_lst str (LambdaSimple'(str_lst2,exp)) counter is_first same_str)
+  | LambdaSimple'(str_lst1, LambdaOpt'(str_lst2, str1, exp)) -> if is_first
+                                                               then if (List.mem str (str_lst2@[str1]))
+                                                                 then (get_read_lst str (LambdaOpt'(str_lst2, str1, exp)) counter false false)
+                                                                 else (get_read_lst str (LambdaOpt'(str_lst2, str1, exp)) counter false same_str)
+                                                               else  if (List.mem str (str_lst2@[str1]))
+                                                                 then (get_read_lst str (LambdaOpt'(str_lst2, str1, exp)) counter is_first false)
+                                                                 else (get_read_lst str (LambdaOpt'(str_lst2, str1, exp)) counter is_first same_str)
+  | LambdaOpt'(str_lst1, str1, LambdaSimple'(str_lst2,exp)) -> if is_first
+                                                           then if (List.mem str str_lst2)
+                                                                then (get_read_lst str (LambdaSimple'(str_lst2,exp)) counter false false)
+                                                                else (get_read_lst str (LambdaSimple'(str_lst2,exp)) counter false same_str)
+                                                           else  if (List.mem str str_lst2)
+                                                                then (get_read_lst str (LambdaSimple'(str_lst2,exp)) counter is_first false)
+                                                                else (get_read_lst str (LambdaSimple'(str_lst2,exp)) counter is_first same_str)
+  | LambdaOpt'(str_lst1, str1, LambdaOpt'(str_lst2,str2,exp)) -> if is_first
+                                                               then if (List.mem str (str_lst2@[str1]))
+                                                                 then (get_read_lst str (LambdaOpt'(str_lst2, str1, exp)) counter false false)
+                                                                 else (get_read_lst str (LambdaOpt'(str_lst2, str1, exp)) counter false same_str)
+                                                               else  if (List.mem str (str_lst2@[str1]))
+                                                                 then (get_read_lst str (LambdaOpt'(str_lst2, str1, exp)) counter is_first false)
+                                                                 else (get_read_lst str (LambdaOpt'(str_lst2, str1, exp)) counter is_first same_str)
+  | LambdaSimple'(str_lst, exp1) ->  if is_first
+                                     then (get_read_lst str exp1 counter is_first same_str)
+                                     else if (List.mem str str_lst)
+                                          then (get_read_lst str exp1 counter is_first false)
+                                          else (get_read_lst str exp1 counter is_first same_str)
+  | LambdaOpt'(str_lst, str1, exp1) -> if is_first
+                                     then (get_read_lst str exp1 counter is_first same_str)
+                                     else if (List.mem str (str_lst@[str1]))
+                                          then (get_read_lst str exp1 counter is_first false)
+                                          else (get_read_lst str exp1 counter is_first same_str)
+  (* TODO WOULD PROBABLY NEED TO DEAL WITH BOX SET BOX GET AND BOX PROBABLY LEAVE THEM THE SAME*)
+  (* Tecnecly i don't think i actually will need in this point*)
+  | _ -> raise X_syntax_error
+  and seq_read_lst str exp_lst counter is_first same_str =
+     match exp_lst with
+     | [] -> []
+     | LambdaSimple'(str_lst, exp) :: t -> if is_first
+                                         then (get_read_lst str exp (counter+1) false (not(List.mem str str_lst)))@(seq_read_lst str t (counter+1) is_first same_str)
+                                         else if (List.mem str str_lst)
+                                              then (get_read_lst str exp counter is_first false)@(seq_read_lst str t counter is_first same_str)
+                                              else (get_read_lst str exp counter is_first same_str)@(seq_read_lst str t counter is_first same_str)
+     | LambdaOpt'(str_lst, str1, exp) :: t -> if is_first
+                                         then (get_read_lst str exp (counter+1) false (not(List.mem str (str_lst@[str1]))))@(seq_read_lst str t (counter+1) is_first same_str)
+                                         else if (List.mem str (str_lst@[str1]))
+                                              then (get_read_lst str exp counter is_first false)@(seq_read_lst str t counter is_first same_str)
+                                              else (get_read_lst str exp counter is_first same_str)@(seq_read_lst str t counter is_first same_str)
+     | h :: t -> (get_read_lst str h counter is_first same_str)@(seq_read_lst str t counter is_first same_str)
+  and get_write_lst str lambda counter is_first =
+    raise X_syntax_error
+  and box_lambda_and_send_recursivly lambda lst =
+  (*TODO THIS FUNCTION CHANGES THE BODY OF THE LAMBDA ACCORDING TO INSTRUCTIONS IN THE FORUM AND SEND TO THE BOX FUNCTION RECURSIVLY*)
+    raise X_syntax_error;;
+                                        
+
+
+(*let rec tail_calls e in_tp =
   match e with
   | Const'(e)-> Const'(e)
   | If' (exp1 , exp2, exp3) ->  If'( (tail_calls exp1 false) , (tail_calls exp2 in_tp) , (tail_calls exp3 in_tp))
@@ -119,12 +239,15 @@ let rec tail_calls e in_tp =
   | []-> Nil
   | _-> (List.rev (List.tl (List. rev lst)))
   ;;
+*)
 
 let annotate_lexical_addresses e = (lexical e [] []) ;;
  
-let annotate_tail_calls e = (tail_calls e false);;
+(*let annotate_tail_calls e = (tail_calls e false);;*)
 
-let box_set e = raise X_not_yet_implemented;;
+let annotate_tail_calls e = raise X_syntax_error;; (* TODO DON'T FORGET TO DELETE *)
+
+let box_set e = (box e);;
 
 let run_semantics expr =
   box_set
